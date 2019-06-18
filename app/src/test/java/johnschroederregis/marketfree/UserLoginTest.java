11a1d25c54c0a2cc48fd5823117466b676a8e836
John Schroeder <JohnSchroedercs@gmail.com>
2019-06-17 19:07:01 -0600

    Removing unused imports and uploading working hello world with barebones unit test

---
+++ app/src/test/java/johnschroederregis/marketfree/UserLoginTest.java
@@ -0,0 +1,13 @@
+package johnschroederregis.marketfree;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserLoginTest {
+
+    @Test
+    public void onCreate() {
+
+    }
+}
