f2a741beaf5de90999a78ed64f7cdd0c0ef07e43
John Schroeder <johnschroedercs@gmail.com>
2019-09-24 13:34:28 -0600

    researching password and login setup (Credentials with smartlock may be a solution)

--- app/src/main/java/johnschroederregis/marketfree/UserLoginActivity.java
+++ app/src/main/java/johnschroederregis/marketfree/UserLoginActivity.java
@@ -2,17 +2,19 @@ package johnschroederregis.marketfree;
 
 import android.content.Intent;
 import android.os.Bundle;
-
 import android.support.v7.app.AppCompatActivity;
-
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class UserLoginActivity extends AppCompatActivity {
 Button registerButton = null;
 Button loginButton = null;
+String userlogin = null;
+String userPassword = null;
+
 
 
 
@@ -46,8 +48,22 @@ Button loginButton = null;
             @Override
             public void onClick(View view) {
 
-                Intent intent = new Intent(getApplicationContext(), UserMainPageActivity.class);
-                startActivity(intent);
+                TextView tempLogin = findViewById(R.id.loginPageEmailAddress);
+                userlogin = tempLogin.getText().toString();
+                TextView tempPaSS =  findViewById(R.id.loginPagepassWordText);
+                userPassword = tempPaSS.getText().toString();
+
+                if((userlogin.equals("admin"))&& (userPassword.equals("admin"))){
+
+                    Intent intent = new Intent(getApplicationContext(), UserMainPageActivity.class);
+                    startActivity(intent);
+                }
+
+                else{
+
+                    Toast toast = Toast.makeText(getApplicationContext(), "Incorrect login", Toast.LENGTH_LONG);
+                    toast.show();
+                }
             }
         });
 
