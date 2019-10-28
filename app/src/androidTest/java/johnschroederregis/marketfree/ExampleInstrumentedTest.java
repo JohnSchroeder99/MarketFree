3d343d92175da7118eb71c341a10205aef084337
John Schroeder <johnschroedercs@gmail.com>
2019-10-28 17:22:32 -0600

    Android X merge and fixing minute changes

--- app/src/androidTest/java/johnschroederregis/marketfree/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/johnschroederregis/marketfree/ExampleInstrumentedTest.java
@@ -1,11 +1,12 @@
 package johnschroederregis.marketfree;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 import static org.junit.Assert.*;
 
@@ -14,12 +15,12 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
+@RunWith(JUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = InstrumentationRegistry.getInstrumentation().getContext();
 
         assertEquals("johnschroederregis.marketfree", appContext.getPackageName());
     }
