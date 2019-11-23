68a3a049a98cab0f677c4f6f46d91c6d20009370
John <johnschroedercs@gmail.com>
2019-11-23 11:44:22 -0700

    Added Recycler view adapter and have functioning adapter with mock data. Still need to add pictures with mock data and fully utilize recycler_view_item_1_xml layout.Still needs cleaning for several layout warnings.

--- app/src/main/java/johnschroeders/marketfree/ManageSubsciptionsActivity.java
+++ app/src/main/java/johnschroeders/marketfree/ManageSubsciptionsActivity.java
@@ -2,14 +2,27 @@ package johnschroeders.marketfree;
 
 import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.jar.Attributes;
 
 public class ManageSubsciptionsActivity extends AppCompatActivity {
     Button subscriptionsBackButton = null;
     Button addSubScriptionsButton = null;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +31,31 @@ public class ManageSubsciptionsActivity extends AppCompatActivity {
 
         subscriptionsBackButton = findViewById(R.id.manageSubscriptionsBackButton);
         addSubScriptionsButton = findViewById(R.id.manageSubscriptionsaddButton);
+
+        Log.d("Manage", "before arraylist made");
+
+
+        ArrayList<String> names = new ArrayList<>();
+        names.add("John");
+        names.add("Deborah");
+        names.add("Useless");
+        names.add("Brandumb");
+        names.add("Richard");
+        names.add("Idiot");
+
+        Log.d("Manage", "after arraylist made");
+
+        RecyclerView recyclerView = findViewById(R.id.manageSubscriptionsView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        Log.d("Manage", "recyclerlayout set");
+        mAdapter = new MyRecyclerViewAdapter(this, names);
+        Log.d("Manage", "adapter initialized");
+        recyclerView.setAdapter(mAdapter);
+        Log.d("Manage", "adapter made");
+
+
+
+
 
 
         subscriptionsBackButton.setOnClickListener(new View.OnClickListener() {
@@ -38,5 +76,9 @@ public class ManageSubsciptionsActivity extends AppCompatActivity {
         });
 
     }
+
+
+
+
 
 }
